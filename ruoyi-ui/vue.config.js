78e0b3497e683fe830662a09d5cdde9690fb5d35
xuanqizi <1318135982@qq.com>
2021-06-18 15:36:44 +0800

    将前端修改为可以部署的配置

--- ruoyi-ui/vue.config.js
+++ ruoyi-ui/vue.config.js
@@ -8,7 +8,7 @@ function resolve(dir) {
 
 const name = defaultSettings.title || '若依管理系统' // 标题
 
-const port = process.env.port || process.env.npm_config_port || 80 // 端口
+const port = process.env.port || process.env.npm_config_port || 2000 // 端口
 
 // vue.config.js 配置说明
 //官方vue.config.js 参考文档 https://cli.vuejs.org/zh/config/#css-loaderoptions
@@ -17,7 +17,8 @@ module.exports = {
   // 部署生产环境和开发环境下的URL。
   // 默认情况下，Vue CLI 会假设你的应用是被部署在一个域名的根路径上
   // 例如 https://www.ruoyi.vip/。如果应用被部署在一个子路径上，你就需要用这个选项指定这个子路径。例如，如果你的应用被部署在 https://www.ruoyi.vip/admin/，则设置 baseUrl 为 /admin/。
-  publicPath: process.env.NODE_ENV === "production" ? "/" : "/",
+  // publicPath: process.env.NODE_ENV === "production" ? "/" : "/",
+  publicPath: './',
   // 在npm run build 或 yarn build 时 ，生成文件的目录名称（要和baseUrl的生产环境路径一致）（默认dist）
   outputDir: 'dist',
   // 用于放置生成的静态资源 (js、css、img、fonts) 的；（项目打包之后，静态资源会放在这个文件夹下）
